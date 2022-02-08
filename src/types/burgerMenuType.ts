7fa9bc0b4b1052481077ddbba6215e6cc68aea84
Yena-Yun <cheryleduart@gmail.com>
2022-02-08 21:39:55 +0900

    :bandage: 세미콜론 수정

--- src/types/burgerMenuType.ts
+++ src/types/burgerMenuType.ts
@@ -1,5 +1,5 @@
 export interface BurgerMenuType {
-  openMenu:boolean,
-  setOpenMenu: ((openMenu:boolean) => void)
-  node: React.RefObject<HTMLDivElement>
+  openMenu: boolean;
+  setOpenMenu: (openMenu: boolean) => void;
+  node: React.RefObject<HTMLDivElement>;
 }
