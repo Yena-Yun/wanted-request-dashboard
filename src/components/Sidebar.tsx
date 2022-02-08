9bbb938a1b2f7bebc53c2d57da5e2af00ef6157b
minjuice1 <minjuice93@gmail.com>
2022-02-08 17:18:33 +0900

    :sparkles: mobile버전 사이드 컴포넌트 추가

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,62 @@
+import styled from 'styled-components';
+import mobile_logo from 'assets/mobile_logo.png';
+import mobile_company from 'assets/mobile_company.png';
+
+const Sidebar = (props:{openMenu:boolean}) => {
+
+  console.log(props.openMenu);
+
+  return (
+    <Wrapper openMenu={props.openMenu} >
+      <Header>
+        <MainLogo src={mobile_logo} alt='mobile_logo'/>
+      </Header>
+      <Section>
+        <Contents><CompanyLogo src={mobile_company} alt='mobile_company'/>파트너정밀가공</Contents>
+        <Contents>로그아웃</Contents>
+      </Section>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.nav<{openMenu: boolean}>`
+  display: flex;
+  flex-direction: column;
+  height: 730px;
+  background: red;
+  transform: ${({ openMenu }) => openMenu ? 'translateX(0%)' : 'translateX(-100%)'};
+  height: 100vh;
+  text-align: left;
+  position: absolute;
+  top: 0;
+  left: 0;
+  transition: all 0.3s ease-out;
+`;
+
+const Header = styled.div`
+  width: 280px;
+  padding: 16px 20px;
+  border-bottom: 1px solid #E5E5E5;
+`
+
+const MainLogo = styled.img`
+  height: 12px;
+`
+
+const Section = styled.section`
+  margin-top: 36px;
+`
+
+const Contents = styled.div`
+  margin-bottom: 24px;
+  padding-left: 32px;
+`
+
+const CompanyLogo = styled.img`
+  width: 15px;
+  height: 15px;
+  margin-right: 8px;
+`
+
+
+export default Sidebar;
