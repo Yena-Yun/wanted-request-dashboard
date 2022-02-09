02a44a1e1ffa361ea359ddc98a6dfe15f1a29f0c
Yena-Yun <cheryleduart@gmail.com>
2022-02-09 13:44:09 +0900

    api 주소 수정

--- src/utils/getApi.ts
+++ src/utils/getApi.ts
@@ -3,7 +3,7 @@ import { dataType } from 'types';
 
 const getApiFunc = async (): Promise<dataType[] | undefined> => {
   try {
-    const getData = await axios.get('http://localhost:8000/requests');
+    const getData = await axios.get('https://luckyzzang-dashboard.herokuapp.com/');
     return getData.data;
   } catch (err) {
     alert(err);
