780e15120e0fc5649e5e14541718bfd02ddfeae1
Ahn <81910935+BYEONGJIN-AHN@users.noreply.github.com>
2022-02-09 15:44:27 +0900

    deploy

--- src/utils/getApi.ts
+++ src/utils/getApi.ts
@@ -3,7 +3,9 @@ import { dataType } from 'types';
 
 const getApiFunc = async (): Promise<dataType[] | undefined> => {
   try {
-    const getData = await axios.get('http://localhost:8000/requests');
+    const getData = await axios.get(
+      'https://ateam-ventrues-mission.herokuapp.com/requests'
+    );
     return getData.data;
   } catch (err) {
     alert(err);
